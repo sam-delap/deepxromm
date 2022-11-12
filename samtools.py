a8be780ef7f956967a238d688eaed6d3194b0ca1
sam-delap <sjcdelap@gmail.com>
2022-11-12 12:11:39 -0500

    finished YAML project generation

--- samtools.py
+++ samtools.py
@@ -1,6 +1,9 @@
 # Import packages
 import os
 import deeplabcut
+from deeplabcut.utils import xrommtools
+import cv2
+import numpy as np
 from ruamel.yaml import YAML
 
 def create_new_project(working_dir=os.getcwd(), experimenter='Johnny Appleseed'):
@@ -14,9 +17,16 @@ def create_new_project(working_dir=os.getcwd(), experimenter='Johnny Appleseed')
 
     config = open("project_config.yaml", 'w')
 
-    yaml = YAML(typ="safe")
-    task = working_dir.split("/")[len(working_dir.split("/")) - 1]
-    path_config_file = deeplabcut.create_new_project(task, experimenter, list(working_dir + "/project.yaml"), working_dir + "/", copy_videos=True)
+    # Create a fake video to pass into the deeplabcut workflow
+    frame = np.zeros((480, 480, 3), np.uint8)
+    out = cv2.VideoWriter('dummy.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, (480,480))
+    out.write(frame)
+    out.release()
+
+    # Create a new project
+    yaml = YAML()
+    task = working_dir.split("\\")[len(working_dir.split("\\")) - 1]
+    path_config_file = deeplabcut.create_new_project(task, experimenter, [working_dir + "\\dummy.avi"], working_dir + "\\", copy_videos=True)
     template = f"""
     task: {task}
     experimenter: {experimenter}
@@ -26,9 +36,32 @@ def create_new_project(working_dir=os.getcwd(), experimenter='Johnny Appleseed')
     nframes:
     """
 
-    yaml.dump(template, config)
+    tmp = yaml.load(template)
 
-def tif_to_avi(working_dir):
+    yaml.dump(tmp, config)
+    config.close()
+
+    # Remove temporary files
+    try:
+        os.rmdir(path_config_file[:path_config_file.find("config")] + "labeled-data\\dummy")
+    except FileNotFoundError:
+        pass
+
+    try:
+        os.remove(path_config_file[:path_config_file.find("config")] + "\\videos\\dummy.avi")
+    except FileNotFoundError:
+        pass
+
+    try:
+        os.remove("dummy.avi")
+    except FileNotFoundError:
+        pass
+
+def train_network(working_dir=os.getcwd()):
+    yaml = YAML()
+    project = yaml.load(working_dir + "\\project_config.yaml")
+
+def tif_to_avi(working_dir=os.getcwd()):
     '''Converts training data tif files to AVI videos'''
     XMA_dir = working_dir + "/trainingdata"
     try:
