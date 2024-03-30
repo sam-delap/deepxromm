a362fee229695316fa33cce9214bde311deb06c3
Aivy Tran <94881175+vytran17@users.noreply.github.com>
2024-03-30 18:29:42 -0400

    refactored autocorrector into a seperate class

--- sdlc_xmalab.py
+++ sdlc_xmalab.py
@@ -7,13 +7,13 @@ from subprocess import PIPE, Popen
 
 import cv2
 import deeplabcut
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from PIL import Image
 from ruamel.yaml import YAML
 
 from analyzer import Analyzer
+from autocorrector import Autocorrector
 from network import Network
 from xma_data_processor import XMADataProcessor
 
@@ -227,212 +227,11 @@ def analyze_videos(working_dir=os.getcwd()):
     analyzer = Analyzer(config=project)
     analyzer.analyze_videos()
 
-def autocorrect_trial(working_dir=os.getcwd()): #try 0.05 also
+def autocorrect_trial(working_dir=os.getcwd()):
     '''Do XMAlab-style autocorrect on the tracked beads'''
-    # Open the config
     project = load_project(working_dir)
-
-    # Error if trials directory is empty
-    new_data_path = os.path.join(working_dir, 'trials')
-    trials = [folder for folder in os.listdir(new_data_path) if os.path.isdir(os.path.join(new_data_path, folder)) and not folder.startswith('.')]
-    if len(trials) <= 0:
-        raise FileNotFoundError(f'Empty trials directory found. Please put trials to be analyzed after training into the {working_dir}/trials folder')
-
-    # Establish project vars
-    yaml = YAML()
-    with open(project['path_config_file']) as dlc_config:
-        dlc = yaml.load(dlc_config)
-
-    iteration = dlc['iteration']
-
-    # For each trial
-    for trial in trials:
-        # Find the appropriate pointsfile
-        try:
-            csv = pd.read_csv(new_data_path + '/' + trial + '/' + 'it' + str(iteration) + '/' + trial + '-Predicted2DPoints.csv')
-        except FileNotFoundError:
-            raise FileNotFoundError(f'Could not find predicted 2D points file. Please check the it{iteration} folder for trial {trial}') from None
-        out_name = new_data_path + '/' + trial + '/' + 'it' + str(iteration) + '/' + trial + '-AutoCorrected2DPoints.csv'
-
-        if project['test_autocorrect']:
-            cams = [project['cam']]
-        else:
-            cams = ['cam1', 'cam2']
-
-        # For each camera
-        for cam in cams:
-            csv = autocorrect_video(cam, trial, csv, project, new_data_path)
-
-        # Print when autocorrect finishes
-        if not project['test_autocorrect']:
-            print(f'Done! Saving to {out_name}')
-            csv.to_csv(out_name, index=False)
-
-def autocorrect_video(cam, trial, csv, project, new_data_path):
-    '''Run the autocorrect function on a single video within a single trial'''
-    # Find the raw video
-    video_path = new_data_path + '/' + trial + '/' + trial + '_' + cam + '.avi'
-    video = cv2.VideoCapture(video_path)
-    if not video.isOpened():
-        raise FileNotFoundError(f'Couldn\'t find a video at file path: {video_path}') from None
-
-    if project['test_autocorrect']:
-        video.set(1, project['frame_num'] - 1)
-        ret, frame = video.read()
-        if ret is False:
-            raise IOError('Error reading video frame')
-        path_to_trial = os.path.join(new_data_path, project['trial_name'])
-        autocorrect_frame(path_to_trial, frame, project['cam'], project['frame_num'], csv, project)
-        return csv
-
-    # For each frame of video
-    print(f'Total frames in video: {video.get(cv2.CAP_PROP_FRAME_COUNT)}')
-
-    for frame_index in range(int(video.get(cv2.CAP_PROP_FRAME_COUNT))):
-        # Load frame
-        print(f'Current Frame: {frame_index + 1}')
-        ret, frame = video.read()
-        if ret is False:
-            raise IOError('Error reading video frame')
-        csv = autocorrect_frame(f'{new_data_path}/{trial}', frame, cam, frame_index, csv, project)
-    return csv
-
-def autocorrect_frame(path_to_trial, frame, cam, frame_index, csv, project):
-    '''Run the autocorrect function for a single frame (no output)'''
-    # For each marker in the frame
-    parts_unique = get_bodyparts_from_xma(project, path_to_trial, mode='2D')
-    if project['test_autocorrect']:
-        parts_unique = [project['marker']]
-    else:
-        parts_unique = get_bodyparts_from_xma(project, path_to_trial, mode='2D')
-    for part in parts_unique:
-        # Find point and offsets
-        x_float = csv.loc[frame_index, part + '_' + cam + '_X']
-        y_float = csv.loc[frame_index, part + '_' + cam + '_Y']
-        x_start = int(x_float-project['search_area']+0.5)
-        y_start = int(y_float-project['search_area']+0.5)
-        x_end = int(x_float+project['search_area']+0.5)
-        y_end = int(y_float+project['search_area']+0.5)
-
-        subimage = frame[y_start:y_end, x_start:x_end]
-
-        subimage_filtered = filter_image(subimage, project['krad'], project['gsigma'], project['img_wt'], project['blur_wt'], project['gamma'])
-
-        subimage_float = subimage_filtered.astype(np.float32)
-        radius = int(1.5 * 5 + 0.5) #5 might be too high
-        sigma = radius * math.sqrt(2 * math.log(255)) - 1
-        subimage_blurred = cv2.GaussianBlur(subimage_float, (2 * radius + 1, 2 * radius + 1), sigma)
-
-        subimage_diff = subimage_float-subimage_blurred
-        subimage_diff = cv2.normalize(subimage_diff, None, 0,255,cv2.NORM_MINMAX).astype(np.uint8)
-
-        # Median
-        subimage_median = cv2.medianBlur(subimage_diff, 3)
-
-        # LUT
-        subimage_median = filter_image(subimage_median, krad=3)
-
-        # Thresholding
-        subimage_median = cv2.cvtColor(subimage_median, cv2.COLOR_BGR2GRAY)
-        min_val, _, _, _ = cv2.minMaxLoc(subimage_median)
-        thres = 0.5 * min_val + 0.5 * np.mean(subimage_median) + project['threshold'] * 0.01 * 255
-        _, subimage_threshold =  cv2.threshold(subimage_median, thres, 255, cv2.THRESH_BINARY_INV)
-
-        # Gaussian blur
-        subimage_gaussthresh = cv2.GaussianBlur(subimage_threshold, (3,3), 1.3)
-
-        # Find contours
-        contours, _ = cv2.findContours(subimage_gaussthresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE, offset=(x_start,y_start))
-        contours_im = [contour-[x_start, y_start] for contour in contours]
-
-        # Find closest contour
-        dist = 1000
-        best_index = -1
-        detected_centers = {}
-        for i, cnt in enumerate(contours):
-            detected_center, _ = cv2.minEnclosingCircle(cnt)
-            dist_tmp = math.sqrt((x_float - detected_center[0])**2 + (y_float - detected_center[1])**2)
-            detected_centers[round(dist_tmp, 4)] = detected_center
-            if dist_tmp < dist:
-                best_index = i
-                dist = dist_tmp
-
-        if project['test_autocorrect']:
-            print('Raw')
-            show_crop(subimage, 15)
-
-            print('Filtered')
-            show_crop(subimage_filtered, 15)
-
-            print(f'Blurred: {sigma}')
-            show_crop(subimage_blurred, 15)
-
-            print('Diff (Float - blurred)')
-            show_crop(subimage_diff, 15)
-
-            print('Median')
-            show_crop(subimage_median, 15)
-
-            print('Median filtered')
-            show_crop(subimage_median, 15)
-
-            print('Threshold')
-            show_crop(subimage_threshold, 15)
-
-            print('Gaussian')
-            show_crop(subimage_threshold, 15)
-
-            print('Best Contour')
-            detected_center_im, _ = cv2.minEnclosingCircle(contours_im[best_index])
-            show_crop(subimage, 15, contours = [contours_im[best_index]], detected_marker = detected_center_im)
-
-        # Save center of closest contour to CSV
-        if best_index >= 0:
-            detected_center, _ = cv2.minEnclosingCircle(contours[best_index])
-            csv.loc[frame_index, part + '_' + cam + '_X']  = detected_center[0]
-            csv.loc[frame_index, part + '_' + cam + '_Y']  = detected_center[1]
-    return csv
-
-def filter_image(image, krad=17, gsigma=10, img_wt=3.6, blur_wt=-2.9, gamma=0.10):
-    '''Filter the image to make it easier to see the bead'''
-    krad = krad*2+1
-    # Gaussian blur
-    image_blur = cv2.GaussianBlur(image, (krad, krad), gsigma)
-    # Add to original
-    image_blend = cv2.addWeighted(image, img_wt, image_blur, blur_wt, 0)
-    lut = np.array([((i/255.0)**gamma)*255.0 for i in range(256)])
-    image_gamma = image_blend.copy()
-    im_type = len(image_gamma.shape)
-    if im_type == 2:
-        image_gamma = lut[image_gamma]
-    elif im_type == 3:
-        image_gamma[:,:,0] = lut[image_gamma[:,:,0]]
-        image_gamma[:,:,1] = lut[image_gamma[:,:,1]]
-        image_gamma[:,:,2] = lut[image_gamma[:,:,2]]
-    return image_gamma
-
-def show_crop(src, center, scale=5, contours=None, detected_marker=None):
-    '''Display a visual of the marker and Python's projected center'''
-    if len(src.shape) < 3:
-        src = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
-    image = src.copy().astype(np.uint8)
-    image = cv2.resize(image, None, fx=scale, fy=scale, interpolation = cv2.INTER_CUBIC)
-    if contours:
-        overlay = image.copy()
-        scaled_contours = [contour*scale for contour in contours]
-        cv2.drawContours(overlay, scaled_contours, -1, (255,0,0),2)
-        image = cv2.addWeighted(overlay, 0.25, image, 0.75, 0)
-    cv2.drawMarker(image, (center*scale, center*scale), color = (0,255,255), markerType = cv2.MARKER_CROSS, markerSize = 10, thickness = 1)
-    if detected_marker:
-        cv2.drawMarker(image,
-        (int(detected_marker[0]*scale),
-        int(detected_marker[1]*scale)),
-        color = (255,0,0),
-        markerType = cv2.MARKER_CROSS,
-        markerSize = 10,
-        thickness = 1)
-    plt.imshow(image)
-    plt.show()
+    autocorrector = Autocorrector(config=project)
+    autocorrector.autocorrect_trial()
 
 def get_bodyparts_from_xma(project, path_to_trial, mode):
     '''Pull the names of the XMAlab markers from the 2Dpoints file'''
