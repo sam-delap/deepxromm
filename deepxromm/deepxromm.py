cd1be3dbc9f33388d05dd8d2c38be7efddcc55e3
Sam DeLap <sjcdelap@gmail.com>
2025-12-25 19:19:18 -0500

    Adding dlc_config to network object creation

--- deepxromm/deepxromm.py
+++ deepxromm/deepxromm.py
@@ -61,7 +61,7 @@ class DeepXROMM:
         deepxromm.project = ProjectFactory.load_config(working_dir)
         deepxromm._analyzer = Analyzer(deepxromm.project)
         deepxromm._autocorrector = Autocorrector(deepxromm.project)
-        deepxromm._network = Network(deepxromm.project)
+        deepxromm._network = Network(deepxromm.project, deepxromm.project.dlc_config)
         deepxromm._data_processor = XMADataProcessor(
             deepxromm.project, deepxromm.project.dlc_config
         )
